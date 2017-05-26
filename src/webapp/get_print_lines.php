7c71ba4b8d2754a7c080306d0def72c2aa910daa
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-05-27 00:20:33 +0200

    added fist printer id test

--- src/webapp/get_print_lines.php
+++ src/webapp/get_print_lines.php
@@ -9,7 +9,16 @@ if(isset($_GET['noprinter'])){
 $dont_delete = 1;
 }
 
+$printer_id = -1;
+if(isset($_GET['printer_id'])){
+$printer_id = 1;
+}
+
 $fetchinfo_dev = mysql_query("SELECT * FROM `items` WHERE `print`='1' AND `printed`='0'");
+
+if($printer_id > -1){
+$fetchinfo_dev = mysql_query("SELECT * FROM `items` WHERE `print`='1' AND `printed`='0' AND `printerid`='".$printer_id."'");
+}
 //print only a headline if items are there
 if(mysql_num_rows($fetchinfo_dev) >0){
 $result = "--- ITEM LIST ---" ."\r\n";
@@ -18,7 +27,7 @@ while($row_dev = mysql_fetch_array($fetchinfo_dev)) {
 	 $result = $result .$row_dev['item_count'] ." x ".$row_dev['item_name']. "\r\n";
 
    if($dont_delete==0){
-  $fetchinfo_push = mysql_query("UPDATE `items` SET `printed`='1' WHERE `id`='".$row_dev['id']."'");
+  $fetchinfo_push = mysql_query("UPDATE `items` SET `printed`='1' WHERE `id`='".$row_dev['id']."' AND");
   }
 }
 if(mysql_num_rows($fetchinfo_dev) >0){
