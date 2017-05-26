ee5523a86223f5e0106a18e20d75089039414c53
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-05-27 01:05:40 +0200

    Update buyprinter.sql

--- src/buyprinter.sql
+++ src/buyprinter.sql
@@ -3,24 +3,16 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 23. Mai 2017 um 22:55
--- Server Version: 5.5.54-0+deb8u1
+-- Erstellungszeit: 27. Mai 2017 um 02:02
+-- Server Version: 5.5.55-0+deb8u1
 -- PHP-Version: 5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Datenbank: `buyprinter`
 --
-CREATE DATABASE IF NOT EXISTS `buyprinter` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `buyprinter`;
 
 -- --------------------------------------------------------
 
@@ -34,8 +26,22 @@ CREATE TABLE IF NOT EXISTS `items` (
   `print` int(11) NOT NULL DEFAULT '0',
   `printed` int(11) NOT NULL DEFAULT '0',
   `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `item_count` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
+  `item_count` int(11) NOT NULL DEFAULT '1',
+  `printerid` int(11) NOT NULL DEFAULT '-1'
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `printers`
+--
+
+CREATE TABLE IF NOT EXISTS `printers` (
+`id` int(11) NOT NULL,
+  `printerid` int(11) NOT NULL DEFAULT '-1',
+  `printername` text NOT NULL,
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Indizes der exportierten Tabellen
@@ -48,6 +54,12 @@ ALTER TABLE `items`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indizes für die Tabelle `printers`
+--
+ALTER TABLE `printers`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
@@ -55,7 +67,9 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT für Tabelle `items`
 --
 ALTER TABLE `items`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=56;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=78;
+--
+-- AUTO_INCREMENT für Tabelle `printers`
+--
+ALTER TABLE `printers`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
