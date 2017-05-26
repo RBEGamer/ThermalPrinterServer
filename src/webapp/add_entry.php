47f29adf27b2ff45700e7faebe3edafa026dd1cc
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-05-27 01:23:04 +0200

    added printer id support

--- src/webapp/add_entry.php
+++ src/webapp/add_entry.php
@@ -4,8 +4,14 @@ $verbindung = mysql_connect ($db_host,
 $db_username, $db_password)or die ("keine Verbindung möglich. Benutzername oder Passwort sind falsch");
 mysql_select_db($db_name)or die ("Die Datenbank existiert nicht.");
 
+$printer_id = -1;
+if(isset($_POST['printer_id'])){
+  $printer_id = $_POST['printer_id'];
+}
+
+
 if(isset($_POST['item_name']) && $_POST['item_name'] != "" && isset($_POST['item_count'])){
-$fetchinfo_dev = mysql_query("INSERT INTO `buyprinter`.`items` (`id`, `item_name`, `print`, `printed`, `added_date`, `item_count`) VALUES (NULL, '".$_POST['item_name']."', '0', '0', CURRENT_TIMESTAMP, '".$_POST['item_count']."');");
+$fetchinfo_dev = mysql_query("INSERT INTO `buyprinter`.`items` (`id`, `item_name`, `print`, `printed`, `added_date`, `item_count`, `printerid`) VALUES (NULL, '".$_POST['item_name']."', '0', '0', CURRENT_TIMESTAMP, '".$_POST['item_count']."', '".$printer_id."');");
 header('Location: index.php');
 exit();  
 }else{
