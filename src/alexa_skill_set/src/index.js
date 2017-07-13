ae21ccccfda05eecbdc80d4014a33e5b98c4492c
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-07-13 02:43:57 +0200

    Create index.js

---
+++ src/alexa_skill_set/src/index.js
@@ -0,0 +1,228 @@
+var request = require("request")
+
+
+
+
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+   // if (event.session.application.applicationId !== "amzn1.ask.skill.88529c2d-15fd-4720-b727-9e66b9b5be30") {
+     //   context.fail("Invalid Application ID");
+    // }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    getWelcomeResponse(callback)
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+
+    var intent = intentRequest.intent
+    var intentName = intentRequest.intent.name;
+
+    // dispatch custom intents to handlers here
+    if (intentName == "AddItemIntent") {
+        handleAddItemIntent(intent, session, callback)
+    } else if (intentName == "AMAZON.YesIntent") {
+        handleYesResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.NoIntent") {
+        handleNoResponse(intent, session, callback)
+    } else if (intentName == "AMAZON.HelpIntent") {
+        handleGetHelpRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.StopIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else if (intentName == "AMAZON.CancelIntent") {
+        handleFinishSessionRequest(intent, session, callback)
+    } else if (intentName == "PrintItemIntent") {
+        handlePrintItemIntent(intent, session, callback) 
+    } else {
+        throw "Invalid intent"
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+
+}
+
+// ------- Skill specific logic -------
+
+function getWelcomeResponse(callback) {
+    var speechOutput = "Hallöchen und Willkommen beim EinkaufslistenDrucker"
+
+    var reprompt = "Sage einfach, füge itemname hinzu"
+    var header = "Einkaufslistendrucker"
+    var shouldEndSession = false
+
+    var sessionAttributes = {
+        "speechOutput" : speechOutput,
+        "repromptText" : reprompt
+    }
+
+    callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
+
+}
+
+
+function handlePrintItemIntent(intent, session, callback) {
+    request("http://109.230.230.209/buylist/print.php", function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+      callback(session.attributes, buildSpeechletResponse("SUCCESS", "Deine Einkaufsliste wird ausgedruckt", "END", true))
+});
+
+}
+
+
+function handleAddItemIntent(intent, session, callback) {
+    var item_name = intent.slots.item.value.toLowerCase()
+    request("http://109.230.230.209/buylist/alexa_skill_api.php?item_count=1&item_name=" + item_name, function (error, response, body) {
+  if(body == "1"){
+      callback(session.attributes, buildSpeechletResponse("SUCCESS", item_name+ " wurde hinzugefügt", "END", true))
+  }else{
+      callback(session.attributes, buildSpeechletResponse("FAILEd", "Der Artikel konnte nicht hinzugefügt werden", "END", true))
+  }
+
+});
+
+        
+
+   
+}
+
+function handleYesResponse(intent, session, callback) {
+    var speechOutput = "Great! Which reindeer? You can find out about Dasher, Dancer, Prancer, Vixen, Comet, Cupid, Blitzen, Rudolph, and Olive"
+    var repromptText = speechOutput
+    var shouldEndSession = false
+
+    callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession))
+}
+
+function handleNoResponse(intent, session, callback) {
+    handleFinishSessionRequest(intent, session, callback)
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Ensure that session.attributes has been initialized
+    if (!session.attributes) {
+        session.attributes = {};
+    }
+
+    var speechOutput = "Ich kann dinge die du einkaufen möchtest direkt an deinen Einkauflistendrucker senden" 
+
+    var repromptText = speechOutput
+
+    var shouldEndSession = false
+
+    callback(session.attributes, buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession))
+
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Tschöö", "", true));
+}
+
+
+// ------- Helper functions to build responses for Alexa -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
+
+function capitalizeFirst(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
